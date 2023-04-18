ee746b5f2bf8e8c85c624fdf75f322c709a906df
Coconutat <coconutat@protonmail.com>
2023-04-18 17:16:48 +0800

    Sync the KernelSU Official Script

--- Proto/synckernelsu.sh
+++ Proto/synckernelsu.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+if [ -d KernelSU ];
+then
+    echo "Find KernelSU Floder,will remove it"
+	rm -rf KernelSU
+else
+	echo "KernelSU Floder,good."
+fi
+
+
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
